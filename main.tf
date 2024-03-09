e4d8eabcfc3948002854a1ef99c7845b15bb1598
jainil15 <jainilpatel115@gmail.com>
2024-03-09 21:01:25 +0530

    Added output for public ipv4 of public instances

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ module "vpc" {
 
 module "instance" {
   source  = "app.terraform.io/Jainil-Org/instance/aws"
-  version = "1.0.2"
+  version = "1.0.3"
   env     = local.env
   // ami_id             = data.aws_ami.ubuntu_ami.id
   ami_id             = "ami-03bb6d83c60fc5f7c"
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "public_instance_ip" {
-  value = module.instance.public_instance_id[0]
+  value = module.instance.public_instance_public_ipv4[0]
 }
