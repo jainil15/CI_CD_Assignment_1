1039554cc5a805728f1b9e96701559e678ab4712
jainil15 <jainilpatel115@gmail.com>
2024-03-09 21:16:43 +0530

    Added s3 backend

--- main.tf
+++ main.tf
@@ -7,6 +7,19 @@ terraform {
   #     name = "ForAssignment"
   #   }
   # }
+
+  backend "s3" {
+    bucket  = var.s3_bucket_backend_name
+    region  = var.region
+    encrypt = true
+    profile = var.backend_profile
+    assume_role = {
+      role_arn = var.role_arn
+    }
+
+    dynamodb_table = var.dynamodb_table
+    key            = var.backend_key
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
--- variables.tf
+++ variables.tf
@@ -1,10 +1,35 @@
 variable "private_key" {
   type        = string
   description = "Enter the key value pair for ssh"
-  sensitive = true
+  sensitive   = true
 }
 
 variable "aws_region" {
-  type = string
+  type        = string
   description = "AWS region"
+}
+
+variable "s3_bucket_backend_name" {
+  type        = string
+  description = "Name of the backend s3 bucket"
+}
+
+variable "backend_profile" {
+  type = string
+  description = "Name of the backend profile"
+}
+
+variable "role_arn" {
+  type = string
+  description = "Aws arn for the assume role"
+}
+
+variable "dynamodb_table_backend_name" {
+  type = string
+  description = "Name of the backend dynamodb table"
+}
+
+variable "backend_key" {
+  type = string
+  description = "Path to store the tfstate file in backend"
 }
